a90ed4c78834a314356376eb7f20e34ce76dfe12
GitHub Action <action@github.com>
2021-09-03 14:35:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/nhh/intellijpluginprimer/MyBundle.kt
+++ src/main/kotlin/com/github/nhh/intellijpluginprimer/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nhh.intellijpluginprimer
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/nhh/intellijpluginprimer/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nhh/intellijpluginprimer/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nhh.intellijpluginprimer.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nhh.intellijpluginprimer.MyBundle
 
 class MyApplicationService {
 
